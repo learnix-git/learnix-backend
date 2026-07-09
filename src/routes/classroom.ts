c4f1d38165d7395d852d70b177e0d307204ff1a2
learnix-git <hungnd1112006@gmail.com>
2026-07-09 16:54:11 +0700

    Đang thực hiện chức năng classroom

--- src/routes/classroom.ts
+++ src/routes/classroom.ts
@@ -4,7 +4,14 @@ import { compel } from "../middlewares/auth";
 
 const router = Router();
 
+// GET
 router.get("/", compel, ClassroomController.get_all_classroom);
+
+// POST
 router.post("/", compel, ClassroomController.create_classroom);
+
+// PUT
+
+// DEL
 
 export default router;
